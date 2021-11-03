63e5cb6030deb8499f91fd5df53f106a27837fc1
Melinda622 <melindasahouli@hotmail.com>
2021-11-03 21:51:41 +0100

    SolarSystem.enum SolarSystemMain.java created

---
+++ training-solutions/src/main/java/enumtype/firstenum/solarsystem/SolarSystemMain.java
@@ -0,0 +1,14 @@
+package enumtype.firstenum.solarsystem;
+
+public class SolarSystemMain {
+    public static void main(String[] args) {
+        SolarSystem earth = SolarSystem.EARTH;
+        System.out.println(earth.getMoons());
+
+        SolarSystem jupiter = SolarSystem.JUPITER;
+        System.out.println(jupiter.getMoons());
+
+        SolarSystem saturn = SolarSystem.SATURN;
+        System.out.println(saturn.getMoons());
+    }
+}
