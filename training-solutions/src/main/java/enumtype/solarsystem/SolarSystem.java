f8df5f1500e7d03e6d600c73f081f319d8aa813a
Melinda622 <melindasahouli@hotmail.com>
2021-11-03 21:55:45 +0100

    SolarSystem.enum and SolarSystemMain.java moved to the correct folder

--- training-solutions/src/main/java/enumtype/solarsystem/SolarSystem.java
+++ training-solutions/src/main/java/enumtype/solarsystem/SolarSystem.java
@@ -1,4 +1,4 @@
-package enumtype.firstenum.solarsystem;
+package enumtype.solarsystem;
 
 public enum SolarSystem {
     MERCURY(0), VENUS(0), EARTH(1), MARS(2), JUPITER(79), SATURN(82), URANUS(27), NEPTUNE(14), PLUTO(5);
