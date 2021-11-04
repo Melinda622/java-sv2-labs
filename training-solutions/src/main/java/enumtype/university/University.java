eaa108f5c07934a2187854e4f4765d0d42ce7b4d
Melinda622 <melindasahouli@hotmail.com>
2021-11-04 20:24:14 +0100

    University.java updated

--- training-solutions/src/main/java/enumtype/university/University.java
+++ training-solutions/src/main/java/enumtype/university/University.java
@@ -1,7 +1,7 @@
 package enumtype.university;
 
 public enum University {
-    BME("Budapesti Műszaki és Gazdaságtudományi Egyetem"), CORVINUS("Budapest Corvinus Egyetem"),ELTE("Eőtvös Lóránd Tudományegyetem"),SOTE("Semmelweis Orvostudományi Egyetem");
+    BME("Budapesti Műszaki és Gazdaságtudományi Egyetem"), CORVINUS("Budapest Corvinus Egyetem"), ELTE("Eőtvös Lóránd Tudományegyetem"), SOTE("Semmelweis Orvostudományi Egyetem");
 
     private final String name;
 
