dc8a8a90deaf9f7af162805054f675d05ddc2088
Melinda622 <melindasahouli@hotmail.com>
2021-11-07 15:51:29 +0100

    IceCream.java created

---
+++ training-solutions/src/main/java/stringseparate/IceCream.java
@@ -0,0 +1,26 @@
+package stringseparate;
+
+import com.sun.security.jgss.GSSUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IceCream {
+    public static void main(String[] args) {
+        List<String> iceCreams=new ArrayList<>();
+        iceCreams.add("csoki");
+        iceCreams.add("vanília");
+        iceCreams.add("eper");
+        iceCreams.add("citrom");
+        iceCreams.add("puncs");
+
+        StringBuilder sb=new StringBuilder("Ma kapható: ");
+        for(int i=0;i< iceCreams.size();i++){
+            sb.append(iceCreams.get(i));
+          if(i!= iceCreams.size()-1){
+              sb.append(", ");
+          }
+        }
+        System.out.println(sb.append("\n").append("Gyerekeknek féláron"));
+    }
+}
