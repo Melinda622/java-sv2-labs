ad43f45ed8d736bf5aef375f4ad57013ea670b22
Melinda622 <melindasahouli@hotmail.com>
2021-11-17 19:36:19 +0100

    Calculator.java and CalculatorMain.java created

---
+++ training-solutions/src/main/java/introexceptiontrycatchtrace/Calculator.java
@@ -0,0 +1,20 @@
+package introexceptiontrycatchtrace;
+
+public class Calculator {
+
+    public void add(int number1,int number2){
+        System.out.println(number1+number2);
+    }
+
+    public void subtract(int number1,int number2){
+        System.out.println(number1-number2);
+    }
+
+    public void multiply(int number1,int number2){
+        System.out.println(number1*number2);
+    }
+
+    public void divide(int number1,int number2){
+        System.out.println((double)(number1/number2));
+    }
+}
