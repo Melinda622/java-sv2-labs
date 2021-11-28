469b6ca52f2914b94d80fd6db14260747ec7e48d
Melinda622 <melindasahouli@hotmail.com>
2021-11-28 14:20:21 +0100

    Tracking.java, TrackPoints.java and tracking.csv created

---
+++ training-solutions/src/main/java/introexceptioncause/Tracking.java
@@ -0,0 +1,10 @@
+package introexceptioncause;
+
+import java.util.Arrays;
+
+public class Tracking {
+    public static void main(String[] args) {
+        TrackPoints trackPoints = new TrackPoints();
+        trackPoints.printElevation();
+    }
+}
